28efc251ec5bbcbf8163b2f9639253208bf14629
Victor124263 <72937975+Victor124263@users.noreply.github.com>
2023-09-29 12:34:30 +0300

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout<<"Hello World.This is my first C++ program\n";
+    cout<<"I am very excited and easy to learn C++\n";
+    cout<<"This is a great university";
+}
